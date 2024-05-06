42e6027cbfe739894ad831ea7f7fee80e0ecc5e7
prajwal bn <prajubn6@gmail.com>
2024-05-06 14:33:28 +0530

    change

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+<<<<<<< Updated upstream
 import { LayoutComponent } from './layout/layout.component';
 import { CoreComponent } from './core/core.component';
+=======
+import { LoginComponent } from './login/login.component';
+>>>>>>> Stashed changes
 
 @NgModule({
   declarations: [
     AppComponent,
+<<<<<<< Updated upstream
     LayoutComponent,
     CoreComponent
+=======
+    LoginComponent
+>>>>>>> Stashed changes
   ],
   imports: [
     BrowserModule,
